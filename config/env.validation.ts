2a1769ed46f46821d4f13a20dcc6e80d307468f8
Andrio Sirait <andriopratama16@gmail.com>
2025-02-24 11:20:33 +0700

    chore: init env validation

---
+++ config/env.validation.ts
@@ -0,0 +1,46 @@
+import { plainToInstance } from 'class-transformer';
+import { IsNumber, IsString, validateSync } from 'class-validator';
+
+class EnvironmentVariables {
+  @IsString()
+  DB_HOST: string;
+
+  @IsNumber()
+  DB_PORT: number;
+
+  @IsString()
+  DB_USER: string;
+
+  @IsString()
+  DB_PASS: string;
+
+  @IsString()
+  DB_NAME: string;
+
+  @IsString()
+  DB_SCHEMA: string;
+
+  @IsNumber()
+  DB_CONNECTION_LIMIT: number;
+
+  @IsNumber()
+  DB_POOL_TIMEOUT: number;
+
+  @IsString()
+  DATABASE_URL: string;
+}
+
+export function validate(config: Record<string, unknown>) {
+  const validatedConfig = plainToInstance(EnvironmentVariables, config, {
+    enableImplicitConversion: true,
+  });
+  const errors = validateSync(validatedConfig, {
+    skipMissingProperties: false,
+  });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+
+  return validatedConfig;
+}
