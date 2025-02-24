65c4caf3d6a24f272799f55e5dd6352f13ff1e13
Andrio Sirait <andriopratama16@gmail.com>
2025-02-24 16:26:56 +0700

    feat: add global interceptors and global pipes validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { TransformationInterceptor } from './transform.interceptor';
+import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    })
+  );
+  app.useGlobalInterceptors(new TransformationInterceptor());
+
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
   await app.listen(3000);
 }
 bootstrap();
